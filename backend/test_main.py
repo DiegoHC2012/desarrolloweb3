df5cb84689fb315510e5996cddcbc25c1e8ffd20
hcdiego <hcdiego2012@gmail.com>
2025-09-25 14:53:35 -0600

    ultimos cambios

--- backend/test_main.py
+++ backend/test_main.py
@@ -4,9 +4,12 @@ import pytest
 import mongomock
 from pymongo import MongoClient
 
-from main import app
+import main
 
-client = TestClient(app)
+client = TestClient(main.app)
+fake_mongo_client = mongomock.MongoClient()
+database = fake_mongo_client.practica1
+collection_historial = database.historial
 
 @pytest.mark.parametrize(
     "a, b, resultado",
@@ -21,7 +24,21 @@ client = TestClient(app)
     ]
 )
 
-def test_sumar(a, b, resultado):
+def test_sumar(monkeypatch,a, b, resultado):
+    monkeypatch.setattr(main, "collection_historial", fake_mongo_client)
     response = client.get("/calculadora/sum?a={a}&b={b}")
     assert response.status_code == 200
     assert response.json() == {"a": a, "b": b, "resultado": resultado}
+    assert collection_historial.find_one({"resultado": resultado,"a": a, "b": b})
+
+def test_historial(monkeypatch):
+    monkeypatch.setattr(main, "collection_historial", collection_historial)
+
+    response = client.get("/calculadora/historial")
+    assert response.status_code == 200
+    expected_data = list(collection_historial.find({}, {"_id": 0}))
+
+    print(f"DEBUG: expected_data: {expected_data}")
+    print(f"DEBUG: response.json(): {response.json()}")
+
+    assert response.json() == expected_data
